a786f6dac2bd673e08dba1b40f6572befd1b0f8b
Birk Weiberg <birk@keyframe.org>
2018-11-08 13:10:42 +0100

    new title design

--- make-title.sh
+++ make-title.sh
@@ -7,7 +7,8 @@
 # width
 # height
 
-LOGO='assets/SAPA_Logo_03_EDFI.png'
+SAPA_LOGO='assets/SAPA_Logo_03_EDFI.png'
+MEMORIAV_LOGO='assets/Memoriav.png'
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -52,31 +53,35 @@ fi
 VIDEO_SIZE=${WIDTH}x${HEIGHT}
 REF_HEIGHT=1080
 SCALE=$(echo "scale=6; $HEIGHT/$REF_HEIGHT" | bc)
-MARGIN_LEFT=$(echo "$SCALE*180/1" | bc)
-MARGIN_TOP_BOTTOM=$(echo "$SCALE*120/1" | bc)
+MARGIN_LEFT_RIGHT=$(echo "$SCALE*192/1" | bc)
+MARGIN_TOP_BOTTOM=$(echo "$SCALE*108/1" | bc)
 
-LOGO_HEIGHT=$(echo "$SCALE*200/1" | bc)
-LOGO_SIZE_POSITION="$((3*LOGO_HEIGHT))x$LOGO_HEIGHT+$MARGIN_LEFT+$MARGIN_TOP_BOTTOM"
+SAPA_LOGO_HEIGHT=$(echo "$SCALE*218/1" | bc)
+SAPA_LOGO_SIZE_POSITION="$((10*SAPA_LOGO_HEIGHT))x$SAPA_LOGO_HEIGHT+$MARGIN_LEFT_RIGHT+$((HEIGHT-MARGIN_TOP_BOTTOM-SAPA_LOGO_HEIGHT))"
 
-TEXT_SIZE=$(echo "$SCALE*48/1" | bc)
-# LINE_HEIGHT=$(echo "$SCALE*80/1" | bc)
+MEMORIAV_LOGO_HEIGHT=$(echo "$SCALE*210/1" | bc)
+MEMORIAV_LOGO_SIZE_POSITION="$((4*MEMORIAV_LOGO_HEIGHT))x$MEMORIAV_LOGO_HEIGHT+$MARGIN_LEFT_RIGHT+$MARGIN_TOP_BOTTOM"
+
+TEXT_SIZE=$(echo "$SCALE*38/1" | bc)
 LINE_HEIGHT=80
 
-SIGNATURE_POSITION="$MARGIN_LEFT,930"
-SIGNATURE_POSITION="$MARGIN_LEFT,$(echo "$SCALE*($REF_HEIGHT-150)/1" | bc)"
-
-DONTCOPY_POSITION="$MARGIN_LEFT,600"
+SIGNATURE_POSITION="$((MARGIN_LEFT_RIGHT)),$(echo "$SCALE*276/1" | bc)"
+DONTCOPY_POSITION_1="$MARGIN_LEFT_RIGHT,$(echo "$SCALE*438/1" | bc)"
+DONTCOPY_POSITION_2="$MARGIN_LEFT_RIGHT,$(echo "$SCALE*487/1" | bc)"
+DONTCOPY_POSITION_3="$MARGIN_LEFT_RIGHT,$(echo "$SCALE*536/1" | bc)"
+DONTCOPY_POSITION_4="$MARGIN_LEFT_RIGHT,$(echo "$SCALE*585/1" | bc)"
 
 # create title still
 TITLE_IMAGE="$(mktemp).png"
-convert -size "$VIDEO_SIZE" xc:white -fill black -font ArialUnicode -pointsize "$TEXT_SIZE" \
+convert -size "$VIDEO_SIZE" xc:black -fill white -font ArialUnicode -pointsize "$TEXT_SIZE" -gravity Northeast \
     -draw "text $SIGNATURE_POSITION '$SIGNATURE'" \
-    -draw "text $DONTCOPY_POSITION 'DONT COPY'" \
+    -draw "text $DONTCOPY_POSITION_1 'Jegliches Kopieren ist untersagt!'" \
+    -draw "text $DONTCOPY_POSITION_2 'Toute copie est interdite!" \
+    -draw "text $DONTCOPY_POSITION_3 'È vietata quasiasi riproduzione!'" \
+    -draw "text $DONTCOPY_POSITION_4 'Any copying is prohibited!'" \
 	"$TITLE_IMAGE"
 # add logo
-convert "$TITLE_IMAGE" "$LOGO" -geometry "$LOGO_SIZE_POSITION" -composite "$TITLE_IMAGE"
+convert "$TITLE_IMAGE" "$SAPA_LOGO" -geometry "$SAPA_LOGO_SIZE_POSITION" -composite "$TITLE_IMAGE"
+convert "$TITLE_IMAGE" "$MEMORIAV_LOGO" -geometry "$MEMORIAV_LOGO_SIZE_POSITION" -composite "$TITLE_IMAGE"
 
-# open "$TITLE_IMAGE"
-
-# print image file path
 echo "$TITLE_IMAGE"
