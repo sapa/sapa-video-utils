ecc47ee591fc14dc73ff848b84fd031ef302cdbd
Birk Weiberg <birk@keyframe.org>
2018-11-14 07:50:30 +0100

    aip with md5, mediainfo and ebucore

--- aip.sh
+++ aip.sh
@@ -106,7 +106,67 @@ convert() {
         fi
         # TODO: validate file and log results
         log "$SOURCE >>> $TARGET"
+        document "$TARGET";
     fi
+}
+
+document() {
+    VIDEO_PATH="$1"
+
+    ### md5 ###
+    MD5_FILE="${VIDEO_PATH%.*}.md5"
+    
+    # check if md5 file either doesn't exist or is older than the video file
+    if [ ! -f "$MD5_FILE" ] || [ $(stat -f "%c" "$MD5_FILE") -lt $(stat -f "%c" "$VIDEO_PATH") ]; then
+        # create/overwrite md5 file and replace file path with file name
+        MD5_VALUE=$(md5 "$VIDEO_PATH" | sed "s|$VIDEO_PATH|$VIDEO_FILE|g")
+        echo "$MD5_VALUE" > "$MD5_FILE"
+        MD5_VALUE="${MD5_VALUE: -32}"
+    else
+        MD5_VALUE="-"
+    fi
+    
+    ### mediainfo text file ###
+    MEDIAINFO_FILE="${VIDEO_PATH%.*}.txt"
+
+    # check if text file either doesn't exist or is older than the video file
+    if [ ! -f "$MEDIAINFO_FILE" ] || [ $(stat -f "%c" "$MEDIAINFO_FILE") -lt $(stat -f "%c" "$VIDEO_PATH") ]; then
+        # create/overwrite mediainfo file
+        # TODO: find a better way to determine the mediainfo path
+        /usr/local/bin/mediainfo "$VIDEO_PATH" > "$MEDIAINFO_FILE"
+        # verify file
+        MEDIAINFO_SIZE="$(stat -f "%z" "$MEDIAINFO_FILE")"
+        if [ "$MEDIAINFO_SIZE" == "0" ] || [ "$MEDIAINFO_SIZE" == "" ]; then
+            MEDIAINFO_SIZE="-"
+            rm "$MEDIAINFO_FILE"
+        fi
+    else
+        MEDIAINFO_SIZE="-"
+    fi
+
+    ### EBUCore xml ###    
+    XML_FILE="${VIDEO_PATH%.*}.xml"
+
+    # check if xml file either doesn't exist or is older than the video file
+    if [ ! -f "$XML_FILE" ] || [ $(stat -f "%c" "$XML_FILE") -lt $(stat -f "%c" "$VIDEO_PATH") ]; then
+        # create/overwrite xml file
+        # TODO: find a better way to determine the mediainfo path
+        /usr/local/bin/mediainfo --Output=EBUCore "$VIDEO_PATH" > "$XML_FILE"
+        # verify file
+        XML_SIZE="$(stat -f "%z" "$XML_FILE")"
+        if [ "$XML_SIZE" == "0" ] || [ "$XML_SIZE" == "" ]; then
+            XML_SIZE="-"
+            rm "$XML_FILE"
+        fi
+    else
+        XML_SIZE="-"
+    fi
+
+    # TODO: create log messages
+    # check if either md5 or mediainfo was executed
+    # if [ "$MD5_VALUE" != "-" ] || [ "$XML_SIZE" != "-" ]; then
+    #     log "$(date +"%H:%M:%S"),$VIDEO_PATH,$MD5_VALUE,$XML_SIZE"
+    # fi    
 }
 
 # loop through all given paths
